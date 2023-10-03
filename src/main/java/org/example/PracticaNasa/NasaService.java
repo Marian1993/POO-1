086c5cc9917dd52613fb4269f822abf8f6247708
Maria Antonia <marianmartorell.93@gmail.com>
2023-10-03 22:31:55 +0200

    He intentat fer es gson, no funciona

--- src/main/java/org/example/PracticaNasa/NasaService.java
+++ src/main/java/org/example/PracticaNasa/NasaService.java
@@ -1,37 +1,82 @@
 package org.example.PracticaNasa;
 
 
-import javax.net.ssl.HttpsURLConnection;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import netscape.javascript.JSObject;
+
+import java.io.*;
 import java.net.URL;
 import java.net.HttpURLConnection;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class NasaService {
 
-    private String clave = "hPz3kslKsnnCv9Ki2SsGcJqRQSZbamBmIfLY1MaM";
+    private final String key = "hPz3kslKsnnCv9Ki2SsGcJqRQSZbamBmIfLY1MaM";
 
     public List<Asteroide> getAsteroide(LocalDateTime avui,LocalDateTime ahir) throws IOException {
-        connexio("buid");
-        return null;
+
+        connexio(key);
+
+        JsonArray array = pasarArchivoAJsonArray();
+
+        ArrayList<Asteroide> jasonArray = new Gson().fromJson(array.toString(), ArrayList.class);
+
+        return jasonArray;
+    }
+    private JsonArray pasarArchivoAJsonArray() throws IOException {
+        JsonArray asteroides = new JsonArray();
+        String json;
+
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\maria\\IdeaProjects\\POO-1\\src\\main\\java\\org\\example\\PracticaNasa\\JsonFile.txt"));
+
+        while ((json = br.readLine()) != null){
+            if(json != null && !json.isEmpty()){
+                List<String> objecte = new ArrayList<String>();
+                String[] split = json.split(",");
+                for (int i = 0; i < split.length; i++) {
+
+                    //System.out.println(split[i]);
+                    if(split[i].contains("name")) {
+                        objecte.add(split[i]);
+
+                        objecte.forEach((n) -> System.out.println(n));
+                    }
+
+                }
+            }
+        }
+        return asteroides;
     }
 
-    private void connexio(String api) throws IOException {
-        URL url = new URL("https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-10-02&end_date=2023-10-03&api_key=hPz3kslKsnnCv9Ki2SsGcJqRQSZbamBmIfLY1MaM");
+
+
+    private void connexio(String clau) throws IOException {
+        URL url = new URL("https://api.nasa.gov/neo/rest/v1/feed?start_date=2023-10-02&end_date=2023-10-03&api_key=" + clau);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
         connection.connect();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        File file = new File("C:\\Users\\maria\\IdeaProjects\\POO-1\\src\\main\\java\\org\\example\\PracticaNasa\\JsonFile.txt");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
         String inputLine;
         String outputLine = "";
-        while ((inputLine = in.readLine()) != null){
+        while ((inputLine = br.readLine()) != null){
             outputLine += inputLine;
+
+
+
+
+            bw.write(outputLine);
+            bw.close();
+
         }
-        in.close();
+        br.close();
         System.out.println(outputLine);
     }
 }
